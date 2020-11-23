ccd1d68ad3dc15979083054818ff21a26cd87828
gzstyp <444141300@qq.com>
2020-11-23 23:55:27 +0800

    配置退出登录

--- src/main/java/com/fwtai/config/SecurityConfig.java
+++ src/main/java/com/fwtai/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.fwtai.config;
 import com.fwtai.JWTAuthenticationEntryPoint;
 import com.fwtai.filter.AuthorizationFilter;
 import com.fwtai.filter.LoginAuthFilter;
+import com.fwtai.tool.ToolClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -16,9 +17,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.annotation.Resource;
+
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Resource
+    private ToolClient client;
 
     @Autowired
     @Qualifier("userDetailsServiceImpl")
@@ -46,10 +52,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .authenticated()//登录认证之后才能访问,注意配置顺序,顺序很重要,它和shiro是一样的
             .and()//此时又回到上面第1行的 'authorizeRequests()'
             .formLogin()//表单登录
-            .usernameParameter("userName")//请求时登录账号参数名,即表单的name,若使用了request.getInputStream()，则不好使!!!
-            .passwordParameter("password")//登录密码参数名,若使用了request.getInputStream()，则不好使!!!
             .loginPage("/login.html")//指定登录页面的url[若没有额外的指定的话默认的登录接口就是它,只是页面的get请求,而登录接口是post请求]
             .permitAll()
+            .and()//此时又回到上面第1行的 'authorizeRequests()'
+            .logout()//配置退出登录
+            .logoutUrl("/exit")//退出登录的url,默认是get方式,若要该成post方式请用下面方式处理
+            //.logoutRequestMatcher(new AntPathRequestMatcher("/exit","POST"))//此方式是POST
+            .logoutSuccessHandler((request,response,authentication)->{//实现接口 org.springframework.security.web.authentication.logout.LogoutSuccessHandler 内部类
+                final String json = client.json(200,"退出注销成功");
+                client.responseJson(json,response);
+            })
+            .permitAll()//表示与退出相关的都统统放行
             .and()
             .cors().and().csrf().disable()
             .addFilter(new LoginAuthFilter(authenticationManager()))//登录认证处理
